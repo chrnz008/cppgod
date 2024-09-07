a8ece885a3aa482847d6a9e4d83a26d352c48fbe
chrnz_ <charancuz008@gmail.com>
2024-09-07 13:31:24 +0530

    fu

--- nestedif.cpp
+++ nestedif.cpp
@@ -1,3 +1,6 @@
+/*Write a program to print the grades of a student based on the marks they have obtained provided 
+as input. The student is graded A if marks are greater than 90, B if marks are greater than 70,
+ C if greater than or equal to 40, else F.*/
 #include <iostream>
 using namespace std;
 
