1de5e0d4453283a045c7bac760aefc5d3ccb1182
chrnz_ <charancuz008@gmail.com>
2024-09-07 13:28:12 +0530

    newww

---
+++ nestedif.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int marks;
+	cin>>marks;
+	if(marks>90){cout<<"A";}
+	else if(marks<=90){
+	    if(marks>70){cout<<"B";}
+	    else if(marks<=70){
+	        if(marks>=40){cout<<"C";}
+	        else{cout<<"F";}
+	    }
+            }
+                }
+
