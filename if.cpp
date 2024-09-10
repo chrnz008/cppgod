21437b76dacdccf10c40bb9ded74e57268e799ea
chrnz_ <charancuz008@gmail.com>
2024-09-10 19:21:26 +0530

    hehe

---
+++ if.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{int n;
+    cout<<"Enter your number :";
+    cin>>n;
+    if (n>0)
+    {cout<<"your number is positive";}
+    else if (n<0)
+    {cout<<"your number is negative";}
+    else{cout<<"your number is zero";}}
